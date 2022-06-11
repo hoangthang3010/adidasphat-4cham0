8af7120b6cba962215f4db16f386afefa55273b8
hoangthang3010 <thangminhhoang98@gmail.com>
2022-06-11 12:24:37 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <img src="../assets/image.png" alt="" @click="click">
+    <img class="image custom flying" src="../assets/image.png" alt="" @click="click" :style="[checkgo1]">
     <audio id="yourAudioTag" controls style="display: none">
       <source src="../assets/ting.mp3" type="audio/ogg">
       <source id="" src="../assets/ting.mp3" type="audio/mpeg">
     </audio>
-    <img src="../assets/image2.png" alt="" @click="click2">
+    <img class="image right" src="../assets/image2.png" alt="" @click="click2">
     <audio id="yourAudioTag1" controls style="display: none">
       <source src="../assets/tiengchuongchua_3.mp3" type="audio/ogg">
       <source id="" src="../assets/tiengchuongchua_3.mp3" type="audio/mpeg">
@@ -17,7 +17,12 @@
 export default {
   data () {
     return {
-      // click: true;
+      click1: true,
+    }
+  },
+  computed: {
+    checkgo1() {
+      return this.click1 ? {backgroundImage: 'url(' + require('../assets/go1.png') + ')'} : ''
     }
   },
   methods: {
@@ -36,5 +41,14 @@ export default {
 </script>
 
 <style scoped>
-
+.image{
+  height: 150px;
+  width: 150px;
+  margin: 0 40px;
+}
+.flying{
+  /* cursor: url('https://res.cloudinary.com/dvkugaw3b/image/upload/v1641562028/cursor_ad12rf.png'), auto; */
+  cursor: url('~@/assets/go1.png'), auto;
+  /* background-image: url('../ '); */
+}
 </style>
